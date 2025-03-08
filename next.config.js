17bf2661867377e86604826d30b727ab3701d618
Daniel Shin <daniel@deyshin.com>
2025-03-07 19:24:46 -0800

    fix some issues

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",  // <=== enables static exports
+  output: "export",  // enables static exports
   reactStrictMode: true,
   distDir: 'out',
+  basePath: '/deyshin', // for GitHub Pages deployment
+  images: {
+    unoptimized: true, // disable image optimization for static export
+  },
 };
 
 module.exports = nextConfig;
